c7b5eeb8e890fc4dd0a28bd480574a14d53ad1ee
indurakshita <imathi101@gmail.com>
2024-02-05 12:53:38 +0530

    initial commit

---
+++ history.py
@@ -0,0 +1,49 @@
+import sqlite3
+import threading
+
+class DatabaseHandler:
+    def __init__(self, db_file='history.db'):
+        self.db_file = db_file
+        self.lock = threading.Lock()
+
+    def connect(self):
+        conn = sqlite3.connect(self.db_file)
+        cursor = conn.cursor()
+        return conn, cursor
+
+    def create_table(self):
+        with self.lock:
+            conn, cursor = self.connect()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS history (
+                    id INTEGER PRIMARY KEY,
+                    input TEXT NOT NULL,
+                    output TEXT NOT NULL
+                )
+            ''')
+            conn.commit()
+            conn.close()
+
+    def insert_data(self, input_data, output_data):
+        with self.lock:
+            conn, cursor = self.connect()
+            cursor.execute("INSERT INTO history (input, output) VALUES (?, ?)", (input_data, output_data))
+            conn.commit()
+            conn.close()
+
+    def fetch_all_data(self):
+        with self.lock:
+            conn, cursor = self.connect()
+            cursor.execute("SELECT * FROM history")
+            columns = [column[0] for column in cursor.description]
+            data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+            conn.close()
+            return data
+
+if __name__ == "__main__":
+    db_handler = DatabaseHandler()
+
+    db_handler.create_table()
+    db_handler.insert_data('sample code', 'this code')
+    data = db_handler.fetch_all_data()
+    print("All data:", data)
---
+++ uidesign.py
@@ -0,0 +1,73 @@
+import gradio as gr
+import google.generativeai as google_key
+from history import DatabaseHandler
+
+
+'''What is Gradio - Interface is Gradio's main high-level class, and 
+allows you to create a web-based GUI / demo around a machine learning model (or any Python function) in a 
+few lines of code.'''
+
+db_handler = DatabaseHandler()
+
+google_key.configure(api_key='AIzaSyDoY1gTJWLHkS61QAt7ygpF5qQdixANR2Q')
+ 
+
+def generate_code(prompt):
+    try:
+        models = [m for m in google_key.list_models() if 'generateText' in m.supported_generation_methods]
+       
+        model = models[0].name if models else None
+        if model:
+            completion = google_key.generate_text(
+                model=model,
+                prompt=prompt,
+                temperature=0,
+                max_output_tokens=200,
+            )
+            response = completion.result
+            db_handler.insert_data(prompt, response)
+            return response
+        
+    except Exception as e:
+        return f"Error: {e}"
+
+
+def show_history():
+    
+    history_data = db_handler.fetch_all_data()
+    data= "\n".join([f"Input: {entry['input']}\nOutput: {entry['output']}\n{'-'*50}" for entry in history_data])
+    return data
+
+
+css = """
+#warning {background-color: #FFCCCB}
+
+"""
+
+with gr.Blocks(css=css) as demo:
+    with gr.Row():
+        with gr.Column(scale=25):
+            title = gr.HTML("""
+                <div style="background-color: #FFCCCB; padding: 10px; text-align: center; border-radius: 10px;">
+                    <strong style="font-size: 24px;">History</strong>
+                </div>
+            """)
+            his_out = gr.TextArea(lines=21,elem_id="warning")
+            btn_show_history = gr.Button("Show History")
+            
+            btn_show_history.click(fn=show_history, outputs=his_out)
+                 
+        with gr.Column(scale=75):
+            title = gr.HTML("""
+                <div style="background-color: #e6e6e6; padding: 10px; text-align: center; border-radius: 10px;">
+                    <strong style="font-size: 24px;">CodeGen App</strong>
+                </div>
+            """)
+         
+         
+            out = gr.TextArea(lines=15)
+            inp = gr.Textbox(placeholder="What do you want to search here?")
+            btn_run = gr.Button("Run")
+            btn_run.click(fn=generate_code, inputs=inp, outputs=out)
+           
+demo.launch(share=True)
